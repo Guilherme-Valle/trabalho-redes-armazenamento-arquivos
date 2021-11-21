2ef9634dc4ba99d75022519bba7ccc482756824b
guilherme.valle <guilherme.valle@leanonsystems.com>
2021-11-20 23:47:25 -0300

    Adiciona envio de arquivo e leitura de conteudo no servidor

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
 import socket
+import sys
 
 # Create a socket object
-s = socket.socket()
+client_socket = socket.socket()
 
 # Define the port on which you want to connect
 port = 12345
 
 # connect to the server on local computer
-s.connect(('127.0.0.1', port))
+client_socket.connect(('localhost', port))
+
+file = open('example_file.txt', 'rb')
+
+file_data = file.read(1024)
+
+client_socket.send(file_data)
 
 # receive data from the server and decoding to get the string.
-print(s.recv(1024).decode())
+print(client_socket.recv(1024).decode())
+
 # close the connection
-s.close()
+client_socket.close()
--- server.py
+++ server.py
@@ -4,21 +4,25 @@ server_socket = socket.socket()
 
 port = 12345
 
-server_socket.bind(('', port))
-print ("socket binded to %s" %(port))
+server_socket.bind(('localhost', port))
+print("socket binded to %s" %(port))
 
 server_socket.listen(5)
 print("socket is listening")
 
 while True:
-    c, addr = server_socket.accept()
-    print('Got connection from', addr)
+    connection, address = server_socket.accept()
+    print('Got connection from', address)
 
     # send a thank you message to the client. encoding to send byte type.
-    c.send('Thank you for connecting'.encode())
+    connection.send(f'Welcome to the server, {address[0]} ' .encode())
+
+    recv_data = connection.recv(1024).decode('utf-8')
+
+    print(f'File received: {recv_data}')
 
     # Close the connection with the client
-    c.close()
+    connection.close()
 
     # Breaking once connection closed
     break
