3003ae34763b329e968f2d63c3da71eb01d67920
1uuc4ab <1uuc4asb@gmail.com>
2021-11-23 00:15:17 -0300

    Uplod e Edit finalizados

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, shutil, json, time
+import socket, shutil, json, time, os
 from datetime import datetime
 import threading
 
@@ -14,6 +14,86 @@ print("socket na porta %s" %(PORT))
 server_socket.listen(5)
 print("socket ouvindo")
 
+def adicionar_arquivo_servidores(antigo_registro_gerenciamento, file_info):
+    if antigo_registro_gerenciamento is None:
+        for number in range(int(file_info["copies"])):
+            nome_servidor = "servidores\\armazenamento_servidor_" + str(number)
+            replicar_arquivo(nome_servidor, file_info["filename"], file_info["content"])
+
+    else:
+        if int(antigo_registro_gerenciamento["copies"]) > int(file_info["copies"]):
+            servidores_para_excluir = range(int(file_info["copies"]), int(antigo_registro_gerenciamento["copies"]))
+
+            for numero_servidor in servidores_para_excluir:
+                nome_servidor = "servidores\\armazenamento_servidor_" + str(numero_servidor)
+                caminho_arquivo = os.path.join(nome_servidor, file_info["filename"])
+                os.remove(caminho_arquivo)
+        else:
+            servidores_para_adicionar = range(int(antigo_registro_gerenciamento["copies"]), int(file_info["copies"]))
+            caminho_arquivo_1 = os.path.join("servidores\\armazenamento_servidor_" + str(0), file_info["filename"])
+            for numero_servidor in servidores_para_adicionar:
+                nome_servidor = "servidores\\armazenamento_servidor_" + str(numero_servidor)
+                servidor_existe = os.path.isdir(nome_servidor)
+                if not servidor_existe:
+                    os.mkdir(nome_servidor)
+                shutil.copy2(caminho_arquivo_1, nome_servidor)
+                
+def receber_arquivo(file_info):
+    # Recebe arquivo do cliente
+
+    print('\n\n\nNome do arquivo: {filename}\nCopias: {copies}\nConteúdo: {content}'.format(**file_info))
+
+    arquivo_gerenciamento = open('arquivo_gerenciamento.json', 'r')
+    conteudo_arquivo_gerenciamento = json.loads(arquivo_gerenciamento.read())
+    arquivo_gerenciamento.close()
+
+    
+    arquivo_gerenciamento_escrita = open('arquivo_gerenciamento.json', 'w')
+    novo_registro_gerenciamento = {
+        "filename": file_info["filename"],
+        "copies": file_info["copies"]
+    }
+
+    try:
+        conteudo_arquivo_gerenciamento.append(novo_registro_gerenciamento)
+        adicionar_arquivo_servidores(None, file_info)
+        arquivo_gerenciamento_escrita.write(json.dumps(conteudo_arquivo_gerenciamento))
+        arquivo_gerenciamento_escrita.close()
+    except:
+        arquivo_gerenciamento_escrita.close()
+
+def editar_replicas(file_info):
+    arquivo_gerenciamento = open('arquivo_gerenciamento.json', 'r')
+    conteudo_arquivo_gerenciamento = json.loads(arquivo_gerenciamento.read())
+    arquivo_gerenciamento.close()
+
+    indice_existente = next(iter([i for i, item in enumerate(conteudo_arquivo_gerenciamento) if item["filename"] == file_info["filename"]]), None)
+    antigo_registro_gerenciamento = conteudo_arquivo_gerenciamento[indice_existente]
+    adicionar_arquivo_servidores(antigo_registro_gerenciamento, file_info)
+    
+    try:
+        novo_registro_gerenciamento = {
+            "filename": file_info["filename"],
+            "copies": file_info["copies"]
+        }
+        conteudo_arquivo_gerenciamento[indice_existente] = novo_registro_gerenciamento
+        arquivo_gerenciamento_escrita = open('arquivo_gerenciamento.json', 'w')
+        arquivo_gerenciamento_escrita.write(json.dumps(conteudo_arquivo_gerenciamento))
+        arquivo_gerenciamento_escrita.close()
+    except:
+        arquivo_gerenciamento_escrita.close()
+
+def replicar_arquivo(nome_servidor, nome_arquivo, conteudo_arquivo):
+    servidor_existe = os.path.isdir(nome_servidor)
+    if not servidor_existe:
+        os.mkdir(nome_servidor)
+
+    caminho_arquivo = os.path.join(nome_servidor, nome_arquivo)
+    escrevendo_arquivo = open(caminho_arquivo, 'wb')
+
+    escrevendo_arquivo.write(conteudo_arquivo.encode('utf8'))
+    escrevendo_arquivo.close()
+    
 
 def save_log(action):
     with open(LOG_FILENAME, 'a+') as file:
@@ -38,7 +118,11 @@ def handle_client(message, addr):
         return
 
 def handle_message(message):
-    print(message)
+    message_json = json.loads(message)
+    if message_json["method"] == 'upload':
+        receber_arquivo(message_json)
+    if message_json["method"] == 'edit':
+        editar_replicas(message_json)
 
 while True:
     connection, address = server_socket.accept()
