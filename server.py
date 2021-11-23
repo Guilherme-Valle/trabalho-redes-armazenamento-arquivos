2b78102162f97101708eb241494cdafe8a1b848d
guilherme.valle <guilherme.valle@leanonsystems.com>
2021-11-22 23:07:22 -0300

    Adiciona log

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket, shutil, json, time
+from datetime import datetime
 import threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 PORT = 4444
 BYTES_PER_MESSAGES = 4096
+LOG_FILENAME = 'log.txt'
 
 server_socket.bind(('localhost', PORT))
 print("socket na porta %s" %(PORT))
@@ -13,9 +15,21 @@ server_socket.listen(5)
 print("socket ouvindo")
 
 
+def save_log(action):
+    with open(LOG_FILENAME, 'a+') as file:
+        action = json.loads(action)
+        log = 'Operation: ' + action['method'] + ';' + \
+              'Filename: ' + action['filename'] + ';' + \
+              'Copies: ' + str(action['copies']) + ';' + \
+              'Datetime: ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"
+        file.write(log)
+        file.close()
+
+
 def handle_client(message, addr):
     try:
        response = handle_message(message)
+       save_log(message)
     except BrokenPipeError:
         print('[DEBUG] addr:', addr, 'Connection closed by client?')
     except Exception as ex:
